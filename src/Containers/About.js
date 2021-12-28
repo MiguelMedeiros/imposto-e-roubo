8349c1d360a8f292722ba392ca28fca6923f2e02
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2021-12-27 21:55:23 -0300

    refactor: remove unused variables

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -1,13 +1,4 @@
-import {
-  faFacebook,
-  faGithub,
-  faTwitter,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, Container, Grid, Typography } from "@material-ui/core";
-import { Tooltip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import GitHubButton from "react-github-btn";
